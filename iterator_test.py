76a352786521c680df4f4c953b53ebdd0f6d1303
Marcus Chen <Marcus@MarcusdeMacBook-Pro.local>
2018-04-13 13:55:52 +0800

    first file

---
+++ iterator_test.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+import sys
+
+list=[1,2,3,4] #create iterator object
+it = iter(list)
+
+#for x in it:
+#	print(x, end=" ")   #output the next element of the iterator
+
+while True:
+	try:
+		print(next(it))
+	except StopIteration:
+		sys.exit()

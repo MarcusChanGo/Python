9ec12ace8d4984bb4083f6046ffada821302df23
MarcusChan <chenxinming_hz@163.com>
2019-11-26 17:31:05 +0800

    add viewBar

--- time_bar.py
+++ time_bar.py
@@ -46,5 +46,22 @@ from tqdm import tqdm
 #         time.sleep(0.01)
 
 
+
+# def viewBar(i):
+#     """
+#     进度条效果
+#     :param i:
+#     :return:
+#     """
+#     output = sys.stdout
+#     for count in range(0, i + 1):
+#         second = 0.1
+#         sleep(second)
+#         output.write('\rcomplete percent:%.0f%%' % count)
+#     output.flush()
+
+# viewBar(200)
+
+
 for i in tqdm(range(1000)):
     pass
